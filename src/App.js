b1b8993a91e5842ae2ec4ca53418adb788ba414d
karthi23Hub <karthikeyanarumugam23@gmail.com>
2024-01-08 23:16:08 +0530

    Carousels slider creded

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './navBar/NavBar';
 import Home from './modules/Home';
 import Product from './modules/Product';
--- src/modules/Home.jsx
+++ src/modules/Home.jsx
@@ -1,9 +1,41 @@
 import React from "react";
-
-const Home = () =>{
-    return(
-        <div>
-            Welcome to home page
+import './Home.css';
+import Carousel from 'react-bootstrap/Carousel';
+// import ExampleCarouselImage from 'components/ExampleCarouselImage';
+const Home = () => {
+    return (
+        <div className="carousel-container">
+            <Carousel >
+                <Carousel.Item>
+                    {/* <ExampleCarouselImage text="First slide" /> */}
+                    <img src={require('../imag/cashew-nuts-wallpaper-image1.jpg')} alt="cashew-nut-image"/>
+                    <Carousel.Caption>
+                        <h3>First slide label</h3>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    {/* <ExampleCarouselImage text="Second slide" /> */}
+                    <img  src={require('../imag/cashew-nuts-wallpaper-image2.jpg')} alt="cashew-nut-image"/>
+                    <Carousel.Caption>
+                        <h3>Second slide label</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                <img src={require('../imag/cashew-nuts-wallpaper-image6.jpg')} alt="cashew-nut-image"/>
+                    {/* <ExampleCarouselImage text="Third slide" /> */}
+                    <Carousel.Caption>
+                        <h3>Third slide label</h3>
+                        <p>
+                            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                        </p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+            <div>
+                
+            </div>
         </div>
     )
 }
