2aa2dbca4efde873b6bc5d765f2cc23f67bd2610
karthi23Hub <karthikeyanarumugam23@gmail.com>
2024-01-23 19:14:45 +0530

    Browser router removed for react scroll

--- src/App.js
+++ src/App.js
@@ -1,30 +1,24 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './navBar/NavBar';
 import Home from './modules/Home';
 import Product from './modules/Product';
+import Process from './modules/Process';
 import About from './modules/About';
 import Contact from './modules/Contact';
 import Footer from './footer/Footer';
-import Process from './modules/Process';
+
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <NavBar />
-        <Routes>
-          <Route exact path='/' element={<Home />} />
-          <Route exact path='/home' element={<Home />} />
-          <Route exact path='/product' element={<Product />} />
-          <Route exact path='/Process' element={<Process/>} />
-          <Route exact path='/about' element={<About />} />
-          <Route exact path='/contact' element={<Contact />} />
-        </Routes>
-        <Footer/>
-      </BrowserRouter>
-
+      <NavBar/>
+      <Home/>
+      <Product/>
+      <Process/>
+      <About/>
+      <Contact/>
+      <Footer/>
     </div>
   );
 }
