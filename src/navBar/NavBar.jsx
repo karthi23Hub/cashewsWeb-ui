14de6e6e3e176199d026e9e89b4d31eb276bcf01
karthi23Hub <karthikeyanarumugam23@gmail.com>
2024-01-23 19:16:53 +0530

    React scroll link added in navbar componend and css styles changed

--- src/navBar/NavBar.jsx
+++ src/navBar/NavBar.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { useState } from 'react';
-import { Link } from "react-router-dom";
+import  {Link} from "react-scroll";
 import { FiMenu, FiX } from "react-icons/fi";
 import './NavBar.css';
+
 
 const NavBar = () => {
     const [open, setOpen] = useState(false);
@@ -19,18 +20,18 @@ const NavBar = () => {
                 Welcome to Pantruti Cashews
             </div>
             <div className="menu-list">
-                <Link className="logo-container" to="/">
+                <Link className="logo-container" to="/" spy={true} smooth={true} offset={-100} duration={500}>
                     CashewNut
                 </Link>
                 <div className="menu-icon">
                     {open ? <FiX onClick={onClickClose} /> : <FiMenu onClick={onClickOpen} />}
                 </div>
                 <ul className={open ? "nav-links active" : "nav-links"}>
-                    <li ><Link onClick={onClickClose} className="nav-link" to='/home'>Home</Link></li>
-                    <li ><Link onClick={onClickClose} className="nav-link" to={'/Product'}>Product</Link></li>
-                    <li ><Link onClick={onClickClose} className="nav-link" to={'/process'}>Process</Link></li>
-                    <li><Link onClick={onClickClose} className="nav-link" to={'/about'}>About Us</Link></li>
-                    <li ><Link onClick={onClickClose} className="nav-link" to={'/contact'}>Contact</Link></li>
+                    <li ><Link onClick={onClickClose} className="nav-link" to='/' spy={true} smooth={true} offset={-100} duration={500}>Home</Link></li>
+                    <li ><Link onClick={onClickClose} className="nav-link" to='#' spy={true} smooth={true} offset={-100} duration={500}>Product</Link></li>
+                    <li ><Link onClick={onClickClose} className="nav-link" to='process' spy={true} smooth={true} offset={-100} duration={500}>Process</Link></li>
+                    <li><Link onClick={onClickClose} className="nav-link" to='about us' spy={true} smooth={true} offset={-100} duration={500}>About Us</Link></li>
+                    <li ><Link onClick={onClickClose} className="nav-link" to='contact' spy={true} smooth={true} offset={-100} duration={500}>Contact</Link></li>
                 </ul>
 
             </div>
