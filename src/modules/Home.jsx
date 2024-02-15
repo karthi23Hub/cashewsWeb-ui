2cc2c13df27a8899ea6c5d84b095a6d504fb3845
karthi23Hub <karthikeyanarumugam23@gmail.com>
2024-02-15 11:02:49 +0530

    Carousel added in home page

--- src/modules/Home.jsx
+++ src/modules/Home.jsx
@@ -1,19 +1,53 @@
 import React from "react";
 import './Home.css';
 import 'animate.css';
-// import Button from "react-bootstrap/Button";
+import Carousel from "react-bootstrap/Carousel";
 
 
 
 const Home = () => {
     return (
         <div className="home-container" id="/">
-            <div className="text-container">
-                <h1 className="animate__backInDown">PandrutiCashews</h1>
-                <p className="animate__backInLeft">We are Providing Best Quality and Affordable Price Contect Me</p>
-                {/* <Button >Rede More</Button> */}
-                <button className=" button animate__backInUp" style={{animationDuration:"2s"}}>reade more</button>
-            </div>
+            <Carousel style={{ width: "100%" }} data-bs-theme="dark">
+            <Carousel.Item>
+                    <img
+                        className="carousel-images"
+                        src={require('../common/imag/Cashew-Apple1.jpg')}
+                        alt="First slide"
+                    />
+                    <Carousel.Caption>
+                        <h5 className="animate__backInDown">Karthi Export</h5>
+                        <p className="animate__backInLeft">  We are providing best quilaty and affordable price</p>
+                        <button className="animate__backInUp">Contact Us</button>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="carousel-images"
+                        src={require('../common/imag/Cashew-Apple2.jpg')}
+                        alt="First slide"
+                    />
+                    <Carousel.Caption>
+                        <h5 className="animate__backInDown">Karthi Export</h5>
+                        <p className="animate__backInLeft">  We are providing best quilaty and affordable price</p>
+                        <button className="animate__backInUp">Contact Us</button>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="carousel-images"
+                        src={require('../common/imag/Cashew-Apple.2.jpg')}
+                        alt="First slide"
+                    />
+                    <Carousel.Caption>
+                        <h5 className="animate__backInDown">Karthi Export</h5>
+                        <p className="animate__backInLeft">
+                            We are providing best quilaty and affordable price
+                        </p>
+                        <button className="animate__backInUp">Contact Us</button>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
         </div>
     )
 }
