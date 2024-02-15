3782898875783f45a1c056d39a1833a5106bab35
karthi23Hub <karthikeyanarumugam23@gmail.com>
2024-02-15 11:06:40 +0530

    Media quiry added in PopUp modal and id changed in product data

--- src/common/popUpModal/PopUpMadal.jsx
+++ src/common/popUpModal/PopUpMadal.jsx
@@ -5,6 +5,12 @@ import { Fab } from "@mui/material";
 import  Close  from "@mui/icons-material/Close";
 
 
+
+const buttonStyle = {
+    borderRadius:"0px 40px"
+}
+
+
 const PopUpModal = (props) => {
 
     let onClose = props.children?.props.onClose;
@@ -21,8 +27,8 @@ const PopUpModal = (props) => {
         ReactDOM.createPortal(<div id="popUp-modal" ref={popUp}>
             <div className="popUp-modal-container">
                 <div className="title-message-container">
-                    karthi
-                    <Fab  size="medium" variant="extended" aria-label="close" onClick={handClick} color="error">
+                    Grade Details
+                    <Fab  size="medium" variant="extended" aria-label="close" onClick={handClick} color="error" style={buttonStyle}> 
                      <Close sx={{mr:0.5}}></Close>close
                 </Fab>  
                 </div>
--- src/common/productData/Data.js
+++ src/common/productData/Data.js
@@ -36,82 +36,148 @@ const ProductData = [
     },
     {
         id: 5,
+        title: "WW-500",
+        price: 700,
+        desc: "White Wholes",
+        img: <img src={require('../imag/cashewNut/ww450-jpeg-500x500.webp')} className='animate__zoomIn' style={{animationDuration:"4s"}}/>
+    },
+    {
+        id: 6,
         title: "SW-180",
         price: 700,
         desc: "Scroched Wholes",
         img: <img src={require('../imag/cashewNut/sw180-500x500.webp')} className='animate__zoomIn' style={{animationDuration:"4s"}}/>
     },
     {
-        id: 6,
+        id: 7,
         title: "SW-210",
         price: 700,
         desc: "Scroched Wholes",
         img: <img src={require('../imag/cashewNut/sw210-plain-cashew-nut-500x500.webp')} className='animate__zoomIn' style={{animationDuration:"4s"}}/>
     },
     {
-        id: 7,
+        id: 8,
         title: "SW-240",
         price: 700,
         desc: "Scroched Wholes",
         img: <img src={require('../imag/cashewNut/sw240-whole-cashew-nuts-500x500.webp')} className='animate__zoomIn' style={{animationDuration:"4s"}}/>
     },
     {
-        id: 8,
+        id: 9,
         title: "SW-320",
         price: 700,
         desc: "Scroched Wholes",
         img: <img src={require('../imag/cashewNut/w240-whole-cashew-nut-500x500.webp')} className='animate__zoomIn' style={{animationDuration:"4s"}}/>
     },
     {
-        id: 9,
+        id: 10,
         title: "SW-450",
         price: 700,
         desc: "Scroched Wholes",
         img: <img src={require('../imag/cashewNut/sw450-cashew-nut-500x500.webp')} className='animate__zoomIn' style={{animationDuration:"4s"}}/>
     },
     {
-        id: 10,
+        id: 11,
+        title: "SW-500",
+        price: 700,
+        desc: "Scroched Wholes",
+        img: <img src={require('../imag/cashewNut/sw450-cashew-nut-500x500.webp')} className='animate__zoomIn' style={{animationDuration:"4s"}}/>
+    },
+    {
+        id: 12,
+        title: "SWS",
+        price: 700,
+        desc: "Scroched Wholes Seconds",
+        img: <img src={require('../imag/cashewNut/sw240-whole-cashew-nuts-500x500.webp')} className='animate__zoomIn' style={{animationDuration:"4s"}}/>
+    },
+
+    {
+        id: 13,
+        title: "DW",
+        price: 700,
+        desc: "Dessert Wholes",
+        img: <img src={require('../imag/cashewNut/sw240-whole-cashew-nuts-500x500.webp')} className='animate__zoomIn' style={{animationDuration:"4s"}}/>
+    },
+    {
+        id: 14,
+        title: "B",
+        price: 700,
+        desc: "Butts",
+        img: <img src={require('../imag/cashewNut/sw240-whole-cashew-nuts-500x500.webp')} className='animate__zoomIn' style={{animationDuration:"4s"}}/>
+    },
+    {
+        id: 15,
+        title: "S",
+        price: 700,
+        desc: "Splits",
+        img: <img src={require('../imag/cashewNut/sw240-whole-cashew-nuts-500x500.webp')} className='animate__zoomIn' style={{animationDuration:"4s"}}/>
+    },
+
+   
+    {
+        id: 16,
+        title: "LWP",
+        price: 700,
+        desc: "Large White Pieces",
+        img: <img src={require('../imag/cashewNut/sw210-plain-cashew-nut-500x500.webp')} className='animate__zoomIn' style={{animationDuration:"4s"}}/>
+    },
+   
+    {
+        id: 17,
         title: "SWP",
         price: 700,
         desc: "Small White Pieces",
         img: <img src={require('../imag/cashewNut/sw210-plain-cashew-nut-500x500.webp')} className='animate__zoomIn' style={{animationDuration:"4s"}}/>
     },
     {
-        id: 11,
-        title: "LWP",
+        id: 18,
+        title: "BB",
         price: 700,
-        desc: "Large White Pieces",
+        desc: "Baby Bits",
         img: <img src={require('../imag/cashewNut/sw210-plain-cashew-nut-500x500.webp')} className='animate__zoomIn' style={{animationDuration:"4s"}}/>
     },
     {
-        id: 12,
-        title: "Splits",
+        id: 19,
+        title: "SB",
         price: 700,
-        desc: "Splits",
+        desc: "Scroched Butts",
         img: <img src={require('../imag/cashewNut/sw210-plain-cashew-nut-500x500.webp')} className='animate__zoomIn' style={{animationDuration:"4s"}}/>
     },
     {
-        id: 13,
-        title: "SWS",
+        id: 20,
+        title: "SS",
         price: 700,
-        desc: "Scroched Wholes Seconds",
+        desc: "Scroche Splits",
         img: <img src={require('../imag/cashewNut/sw210-plain-cashew-nut-500x500.webp')} className='animate__zoomIn' style={{animationDuration:"4s"}}/>
     },
     {
-        id: 14,
+        id: 21,
+        title: "SP",
+        price: 700,
+        desc: "Scroched Pieces",
+        img: <img src={require('../imag/cashewNut/sw210-plain-cashew-nut-500x500.webp')} className='animate__zoomIn' style={{animationDuration:"4s"}}/>
+    },
+    {
+        id: 22,
+        title: "SSP",
+        price: 700,
+        desc: "Scroched Small  Pieces",
+        img: <img src={require('../imag/cashewNut/sw210-plain-cashew-nut-500x500.webp')} className='animate__zoomIn' style={{animationDuration:"4s"}}/>
+    },
+    {
+        id: 23,
         title: "SPS",
         price: 700,
         desc: "Scroched Pieces Seconds",
         img: <img src={require('../imag/cashewNut/sw210-plain-cashew-nut-500x500.webp')} className='animate__zoomIn' style={{animationDuration:"4s"}}/>
     },
     {
-        id: 15,
-        title: "DW",
+        id: 24,
+        title: "DP",
         price: 700,
-        desc: "Dessert Wholes",
-        img: <img src={require('../imag/cashewNut/sw240-whole-cashew-nuts-500x500.webp')} className='animate__zoomIn' style={{animationDuration:"4s"}}/>
+        desc: "Dessert Pieces",
+        img: <img src={require('../imag/cashewNut/sw210-plain-cashew-nut-500x500.webp')} className='animate__zoomIn' style={{animationDuration:"4s"}}/>
     },
-
 ]
 
 export default ProductData;
