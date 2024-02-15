b475aa7f77da9dcfb76e2f2dcabc68ef25bd30f6
karthi23Hub <karthikeyanarumugam23@gmail.com>
2024-02-15 11:09:07 +0530

    image added in about us page

--- src/modules/About.jsx
+++ src/modules/About.jsx
@@ -5,20 +5,23 @@ import './About.css';
 
 
 const About = () => {
+
+
+
     return (
         <div id="about-us">
             <div>
                 <div className="container mb-5">
                     <div className="row">
-                        <div className="col-12 text-center py-4 my-4">
+                        <div className="col-12 text-center py-4 my-4"> 
                             <h1>About Us</h1>
                             <hr />
                         </div>
                     </div>
                     <div className="row">
-                       
+
                         <div className="col-md-6 my-2">
-                            <div className="py-5 my-5 px-2"> 
+                            <div className="py-5 my-5 px-2">
                                 <h2>About Karthi Export</h2>
                                 <p>Karthi Exports is a bankable name that can be trusted for processing retail and massive product requirements in a professional manner.
                                     We started our company operations as an exporter, supplier, and importer of
@@ -29,7 +32,7 @@ const About = () => {
                             </div>
                         </div>
                         <div className="col-md 5 d-flex justify-content-center">
-                            <img src={require('../common/imag/CG-Process/cashewBowl.jpg')} alt="Contact Us" className="contactPage-image" />
+                            <img src={require('../common/imag/smiling-businessmen.webp')} alt="Contact Us" className="contactPage-image" />
                         </div>
                     </div>
                 </div>
