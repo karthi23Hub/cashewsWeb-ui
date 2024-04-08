07cbbca9f4f83a800e84dd9f393e357e358d96b3
karthi23Hub <karthikeyanarumugam23@gmail.com>
2024-04-08 09:39:39 +0530

    Product file removed from the App.js file

--- src/App.js
+++ src/App.js
@@ -8,12 +8,13 @@ import Contact from './modules/Contact';
 import Footer from './footer/Footer';
 
 
+
+
 function App() {
   return (
     <div className="App">
       <NavBar/>
       <Home/>
-      {/* <Product/> */}
       <CashewProduct/>
       <About/>
       <Contact/>
