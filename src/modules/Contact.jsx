6c001f027870472e0cc3a9b8b54c77e3a3186205
karthi23Hub <karthikeyanarumugam23@gmail.com>
2024-04-12 13:56:31 +0530

    Code removed from the Contact.jsx

--- src/modules/Contact.jsx
+++ src/modules/Contact.jsx
@@ -4,50 +4,8 @@ import './Contact.css';
 const Contact = () => {
     return (
         <div id="contact">
-            <div>
-                <div className="container mb-5">
-                    <div className="row">
-                        <div className="col-12 text-center py-4 my-4">
-                            <h1>Our Contact</h1>
-                            <hr />
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-md 5 d-flex justify-content-center">
-                            <img src={require('../common/imag/New folder/cashew-nuts-wallpaper-image3.jpg')} 
-                            data-aos="fade-up"
-                            data-aos-duration="1000"
-                            alt="Contact Us" className="img-fluid"/>
-                        </div>
-                        <div className="col-md-6 my-2" id="contactUs-content">
-                            <div>
-                                <h5>Contact Person:</h5>
-                                <label>Karthikeyan.A</label>
-                            </div>
-                            <div>
-                            <h5>Address:</h5>
-                           <p>
-                             678, Easte Street MutharasanKuppam,Sathipattu(po),<br/> Panruti, Cuddalore(tk), Tamil Nadu 607106.
-                           </p>
-                            </div>
-                            <div>
-                                <h5>Contact:</h5>
-                                <label>+9159116690</label>
-                            </div>
-                            <div>
-                                <h5>Email:</h5>
-                                <label>karthiexports@gmail.com</label>
-                            </div> 
-                            <div>
-                                <h5>Our Time:</h5>
-                                <label>Mon-Sat:09:AM-09-00PM</label><br/>
-                                <label>Sun:Available</label>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+       
+    </div>
     )
 }
 export default Contact;
