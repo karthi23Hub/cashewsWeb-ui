04d94d95a588a463debe4f8e0b6a1494afebb457
karthi23Hub <karthikeyanarumugam23@gmail.com>
2024-04-12 13:59:52 +0530

    CashewBlog file created

---
+++ src/modules/CashewBlog.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './CashewBlog.css';
+
+
+
+
+
+const CashewBlog = () =>{
+    return(
+        <div id="CashewBlog">
+          
+        </div>
+    )
+}
+export default CashewBlog
