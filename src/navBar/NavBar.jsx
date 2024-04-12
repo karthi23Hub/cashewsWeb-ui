7a08ce152e83a6833331a9d017129dc21d7aed54
karthi23Hub <karthikeyanarumugam23@gmail.com>
2024-04-12 13:52:23 +0530

    New menu Blog button added and background color changed

--- src/navBar/NavBar.jsx
+++ src/navBar/NavBar.jsx
@@ -26,6 +26,7 @@ const NavBar = () => {
                 <ul className={open ? "nav-links-active" : "nav-links"}>
                     <li ><Link onClick={onClickClose} className="nav-link" to='/' spy={true} smooth={true} offset={-100} duration={500}>Home</Link></li>
                     <li ><Link onClick={onClickClose} className="nav-link" to='product' spy={true} smooth={true} offset={-60} duration={500}>Product</Link></li>
+                    <li ><Link onClick={onClickClose} className="nav-link" to='CashewBlog' spy={true} smooth={true} offset={-60} duration={500}>Blog</Link></li>
                     <li><Link onClick={onClickClose} className="nav-link" to='about-us' spy={true} smooth={true} offset={-60} duration={500}>About Us</Link></li>
                     <li ><Link onClick={onClickClose} className="nav-link" to='contact' spy={true} smooth={true} offset={-60} duration={500}>Contact</Link></li>
                 </ul>
